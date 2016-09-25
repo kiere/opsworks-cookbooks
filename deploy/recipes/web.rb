91b8598018832bc256411a9ff573bd56d9e5a188
dondenoncourt <dondenoncourt@gmail.com>
2016-09-25 10:19:25 -0400

    Back out test log

--- deploy/recipes/web.rb
+++ deploy/recipes/web.rb
@@ -8,20 +8,17 @@ node[:deploy].each do |application, deploy|
   end
 
   opsworks_deploy_dir do
-    Chef::Log.info("Denoncourt added this test Chef::Log.info in opsworks_deploy_dir block")
     user deploy[:user]
     group deploy[:group]
     path deploy[:deploy_to]
   end
 
   opsworks_deploy do
-    Chef::Log.info("Denoncourt added this test Chef::Log.info in opsworks_deploy block")
     app application
     deploy_data deploy
   end
 
   nginx_web_app application do
-    Chef::Log.info("Denoncourt added this test Chef::Log.info in nginx_web_app block")
     application deploy
     cookbook "nginx"
   end
