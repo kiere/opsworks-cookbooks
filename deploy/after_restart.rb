6074aad9caed52b93086ab067965fe6d5f749220
dondenoncourt <dondenoncourt@gmail.com>
2016-09-25 16:37:42 -0400

    Remove after_restart

--- deploy/after_restart.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.info("Denoncourt After deployment")
-
-#execute "rake assets:precompile" do
- #cwd release_path
- #command "bundle exec rake assets:precompile"
- #environment "RAILS_ENV" => rails_env
-#end
